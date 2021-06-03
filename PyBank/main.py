f9252f6250830eea51aae17c1f9ed735f3293fdf
Laura Zaragoza <laurazaragoza@Lauras-MacBook-Pro.local>
2021-06-02 23:48:43 -0700

    Add updated homework files

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,51 +1,73 @@
+#import modules
 import os
 import csv
 
+# csv path
 budget_data = os.path.join("Resources", "PyBank_budget_data.csv")
 
+#variables
 total_profit = 0
-months = 0
-#DATE = []
+total_date = 0
+counter = 0
+
 row_index_profit = 1
 total_change = 0
 changes = []
 
-
+#read csv file
 with open(budget_data, newline='') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
 
+    #csv file has a header row, skip
     csv_header = next(csv_file)
-    
-    print("Financial Analysis")
-    print("------------------------------")
 
+    #loop variables
     previous_row = 0
     change = 0
     
-
+    
+    #loop through each row to add profits/changes
     for row in csv_reader:
+        
         total_profit += int(row[1])
-        months += 1
-        #date = [str(row[0])]
-        # changes2 =[(row[0])]
-        # final_changes = [changes, changes2]
+        total_date = total_date + 1
+        counter += 1
+        profit = int(row[1])
+        date = row[0]
 
-        if months > 1:
+        if counter > 1:
             change = int(row[1]) - previous_row
-            #date += 1
-            #changes2 = next()
+            
             changes.append(change)
-            #changes2.append(date)
+        
             max_profit = max(changes)
             min_profit = min(changes)
+            
             total_change += change
-            # if changes == max_profit:
-            #     print(f"Greatest Increase in Profits: {row[0]} (${row[1]})")
-        
+
         previous_row = int(row[1])
 
-    print(f"Total Months: {months}")
+output_path = os.path.join("Analysis", "Analysis.txt")       
+with open(output_path, 'w') as txt_file:
+
+    csvwriter = csv.writer(txt_file)
+    
+    csvwriter.writerow([(
+        "Financial Analysis\n"
+        "--------------------------------\n"
+        f"Total Months: {counter}\n"
+        f"Total: $ {total_profit}\n"
+        f"Average Changes: {total_change/(counter-1)}\n"
+        f"Greatest Increase in Profits: {date} (${max_profit})\n"
+        f"Greatest Decrease in Profits: {date} (${min_profit})\n"
+    )])
+
+
+    #print out statements
+    print("Financial Analysis")
+    print("--------------------------------")
+    print(f"Total Months: {counter}")
     print(f"Total: $ {total_profit}")
-    print(f"Average Changes: {total_change/(months-1)}")
-    print(f"Greatest Increase in Profits: {months} (${max_profit})")
-    print(f"Greatest Decrease in Profits: {months} (${min_profit})")
+    print(f"Average Changes: {total_change/(counter-1)}")
+    print(f"Greatest Increase in Profits: {date} (${max_profit})")
+    print(f"Greatest Decrease in Profits: {date} (${min_profit})")
